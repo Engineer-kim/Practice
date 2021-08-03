3bb13d30cadbb52f2a763a808fb35fc4b78ac4bf
kimhanjin0103 <85604001+kimhanjin0103@users.noreply.github.com>
2021-08-03 10:06:06 +0900

    Add files via upload

---
+++ 포스기/Form1.cs
@@ -0,0 +1,177 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace 포스기
+{
+    public partial class Form1 : Form
+    {
+        DataTable table = new DataTable();
+        public Form1()
+        {
+            InitializeComponent();
+            table.Columns.Add("Name", typeof(string));
+            table.Columns.Add("Price", typeof(string));
+            table.Columns.Add("Count", typeof(string));
+            table.Columns.Add("Total", typeof(string));
+
+            dataGridView1.DataSource = table;
+            numericUpDown1.Value = 1;
+        }
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            MySqlConnection connection = new MySqlConnection("datasource=localhost;port=3306;username=root;password=1234;");
+            connection.Open();
+            if (connection.State == ConnectionState.Open)
+            {
+                label5.Text = " Server connect Confirm";
+                label5.ForeColor = Color.Black;
+            }
+            else
+            {
+                label5.Text = "DisConnected";
+                label5.ForeColor = Color.Red;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e) // 담기
+        {
+            if (textBox1.Text == "" || textBox2.Text == "")
+            {
+                MessageBox.Show("항목을 정확히 입력해주세요");
+                textBox1.Clear();
+                textBox2.Clear();
+            }
+            else
+            {
+
+                decimal price = decimal.Parse(textBox2.Text);
+                decimal count = numericUpDown1.Value;
+                decimal total = price * count;
+
+
+                table.Rows.Add(textBox1.Text, textBox2.Text, numericUpDown1.Value, total);
+                dataGridView1.DataSource = table;
+
+
+                textBox1.Clear();
+                textBox2.Clear();
+                numericUpDown1.Value = 1;
+
+
+                decimal all = 0;
+                for (int i = 0; i < dataGridView1.Rows.Count; ++i)
+                {
+                    all += Convert.ToDecimal(dataGridView1.Rows[i].Cells[3].Value);
+                }
+                textBox3.Text = all.ToString();
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e) // 취소
+        {
+            foreach (DataGridViewRow item in this.dataGridView1.SelectedRows)
+            {
+                dataGridView1.Rows.RemoveAt(item.Index);
+            }
+
+            //합계창에 수정된 값 넣기
+            decimal all = 0;
+            for (int i = 0; i < dataGridView1.Rows.Count; ++i)
+            {
+                all += Convert.ToDecimal(dataGridView1.Rows[i].Cells[3].Value);
+            }
+
+            textBox3.Text = all.ToString();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+            using (MySqlConnection conn = new MySqlConnection("Server=localhost;Port=3306;Database=pos_dataset;Uid=root;Pwd=1234"))
+            {
+                conn.Open();
+
+                for (int i = 0; i < dataGridView1.Rows.Count - 1; i++)
+                {
+                    String Name = dataGridView1.Rows[i].Cells[0].Value.ToString();
+                    String Price = dataGridView1.Rows[i].Cells[1].Value.ToString();
+                    String Count = dataGridView1.Rows[i].Cells[2].Value.ToString();
+                    String Total = dataGridView1.Rows[i].Cells[3].Value.ToString();
+
+
+                    string sql = string.Format("INSERT INTO sales_tb(name,price,count,total,c_num) VALUES  ('{0}',{1},{2},{3},{4})", @Name, @Price, @Count, @Total, @i);
+
+
+                    try
+                    {
+                        MySqlCommand command = new MySqlCommand(sql, conn);
+                        command.ExecuteNonQuery();
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show(ex.Message);
+                    }
+                }
+            }
+
+            MessageBox.Show("계산되었습니다.");
+
+
+            int rowCount = dataGridView1.Rows.Count;
+            for (int n = 0; n < rowCount; n++)
+            {
+                if (dataGridView1.Rows[0].IsNewRow == false)
+                    dataGridView1.Rows.RemoveAt(0);
+            }
+
+
+            textBox3.Text = "0";
+        }
+
+        private void button4_Click(object sender, EventArgs e) // 판매 내역으로 이동
+        {
+            Form2 dlg = new Form2();
+            dlg.ShowDialog();
+        }
+        public void LoadData()
+        {
+            string sql = "Server=localhost;Port=3306;Database=pos_dataset;Uid=root;Pwd=1234";
+            MySqlConnection con = new MySqlConnection(sql);
+            MySqlCommand cmd_db = new MySqlCommand("SELECT * FROM sales_tb;", con);
+
+            try
+            {
+                MySqlDataAdapter sda = new MySqlDataAdapter();
+                sda.SelectCommand = cmd_db;
+                DataTable dbdataset = new DataTable();
+                sda.Fill(dbdataset);
+                BindingSource bSource = new BindingSource();
+
+                bSource.DataSource = dbdataset;
+                dataGridView1.DataSource = bSource;
+                sda.Update(dbdataset);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            LoadData();
+        }
+    }
+          
+    }
+
---
+++ 포스기/Form2.cs
@@ -0,0 +1,193 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace 포스기
+{
+    public partial class Form2 : Form
+    {
+        MySqlConnection connection = new MySqlConnection("Server=localhost;Port=3306;Database=pos_dataset;Uid=root;Pwd=1234");
+        MySqlCommand command;
+        MySqlDataAdapter adapter;
+        DataTable table = new DataTable();
+
+        int selectedRow;
+       
+
+
+        public Form2()
+        {
+            InitializeComponent();
+        }
+        public void searchData(string valueToSearch)
+        {
+            string query = "SELECT * FROM sales_tb WHERE CONCAT(`name`, `price`, `count`, `total`) like '%" + valueToSearch + "%'";
+            command = new MySqlCommand(query, connection);
+            adapter = new MySqlDataAdapter(command);
+            table = new DataTable();
+            adapter.Fill(table);
+            dataGridView1.DataSource = table;
+        }
+
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+            searchData("");
+        }
+
+        public void LoadData()
+        {
+            string sql = "Server=localhost;Port=3306;Database=pos_dataset;Uid=root;Pwd=1234";
+            MySqlConnection con = new MySqlConnection(sql);
+            MySqlCommand cmd_db = new MySqlCommand("SELECT * FROM sales_tb;", con);
+
+            try
+            {
+                MySqlDataAdapter sda = new MySqlDataAdapter();
+                sda.SelectCommand = cmd_db;
+                DataTable dbdataset = new DataTable();
+                sda.Fill(dbdataset);
+                BindingSource bSource = new BindingSource();
+
+                bSource.DataSource = dbdataset;
+                dataGridView1.DataSource = bSource;
+                sda.Update(dbdataset);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+            //텍스트 박스 초기화
+            textBox1.Text = "";
+            textBox6.Text = "";
+            textBox2.Text = "";
+            textBox3.Text = "";
+            textBox4.Text = "";
+            textBox5.Text = "";
+        }
+
+        private void button5_Click(object sender, EventArgs e)// 뒤로가기
+        {
+            this.Close();
+        }
+
+        private void button4_Click(object sender, EventArgs e)//새로고침
+        {
+            LoadData();
+        }
+
+        private void button1_Click(object sender, EventArgs e)//검색
+        {
+            if (textBox1.Text == "")
+            {
+                MessageBox.Show("검색 정보를 입력해주세요");
+            }
+            else
+            {
+                string valueToSearch = textBox1.Text.ToString();
+                searchData(valueToSearch);
+                
+                textBox1.Text = "";
+            }
+        }
+
+        private void dataGridView1_CellClick_1(object sender, DataGridViewCellEventArgs e)
+        {
+            selectedRow = e.RowIndex;
+            DataGridViewRow row = dataGridView1.Rows[selectedRow];
+            textBox6.Text = row.Cells[0].Value.ToString();
+            textBox2.Text = row.Cells[1].Value.ToString();
+            textBox3.Text = row.Cells[2].Value.ToString();
+            textBox4.Text = row.Cells[3].Value.ToString();
+            textBox5.Text = row.Cells[4].Value.ToString();
+        }
+
+        private void button2_Click(object sender, EventArgs e) //수정
+        {
+            string constring = "Server=localhost;Port=3306;Database=pos_data;Uid=root;Pwd=1234";
+
+            if (textBox2.Text == "" || textBox3.Text == "" || textBox4.Text == "")
+            {
+                MessageBox.Show("항목을 정확히 입력해주세요");
+                textBox2.Clear();
+                textBox4.Clear();
+                textBox5.Clear();
+            }
+            else
+            {
+                decimal price = decimal.Parse(textBox4.Text);
+                decimal count = decimal.Parse(textBox5.Text);
+                decimal total = price * count;
+
+                textBox5.Text = total.ToString();
+
+                
+                string Query = "update pos_dataset.sales_tb set no ='" + this.textBox6.Text + "',name='" + this.textBox2.Text + "',price='" + this.textBox3.Text + "'," +
+                    "count='" + this.textBox4.Text + "',total='" + this.textBox5.Text + "' where no ='" + this.textBox6.Text + "'";
+                MySqlConnection conDataBase = new MySqlConnection(constring);
+                MySqlCommand cmdDatabase = new MySqlCommand(Query, conDataBase);
+                MySqlDataReader myReader;
+
+                try
+                {
+                    conDataBase.Open();
+                    myReader = cmdDatabase.ExecuteReader();
+                    MessageBox.Show("수정완료");
+
+                    while (myReader.Read())
+                    {
+
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+            }
+            LoadData();
+        }
+
+        private void button3_Click(object sender, EventArgs e)//삭제
+        {
+            string constring = "Server=localhost;Port=3306;Database=pos_dataset;Uid=root;Pwd=1234";
+            if (textBox6.Text == "")
+            {
+                MessageBox.Show("삭제 할 항목을 찾지 못했습니다.");
+            }
+            else
+            {
+                //delete를 통해 DB로 삭제된 데이터 전송 - 기본키 기준으로 삭제위치 탐색
+                string Query = "delete from pos_dataset.sales_tb where no ='" + this.textBox6.Text + "';";
+                MySqlConnection conDataBase = new MySqlConnection(constring);
+                MySqlCommand cmdDatabase = new MySqlCommand(Query, conDataBase);
+                MySqlDataReader myReader;
+
+                try
+                {
+                    conDataBase.Open();
+                    myReader = cmdDatabase.ExecuteReader();
+                    MessageBox.Show("삭제완료");
+
+                    while (myReader.Read())
+                    {
+
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+
+                LoadData();
+            }
+        }
+    }
+}
